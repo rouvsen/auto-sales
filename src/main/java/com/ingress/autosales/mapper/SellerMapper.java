93411d3f9da8235399c87066b7cafcddfb2c68aa
rouvsen <rouvsen@gmail.com>
2024-03-26 21:05:41 +0400

    Add SellerMapper Mapstruct

--- src/main/java/com/ingress/autosales/mapper/SellerMapper.java
+++ src/main/java/com/ingress/autosales/mapper/SellerMapper.java
@@ -1,10 +1,16 @@
 package com.ingress.autosales.mapper;
 
 import com.ingress.autosales.domain.SellerEntity;
+import org.mapstruct.Builder;
 import org.mapstruct.Mapper;
 import com.ingress.autosales.dto.SellerDTO;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
-@Mapper(componentModel = "spring")
+@Mapper(
+        componentModel = "spring",
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        builder = @Builder(disableBuilder = true)
+)
 public interface SellerMapper {
 
     SellerDTO sellerToSellerDTO(SellerEntity seller);
